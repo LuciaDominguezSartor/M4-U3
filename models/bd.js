ee271b4093a10fe4d89a9c12797fdf26cca04fe3
Lucia Dominguez <luciadominguezsartor@gmail.com>
2023-07-20 18:57:55 -0300

    Tarea

---
+++ models/bd.js
@@ -0,0 +1,15 @@
+const { query } = require('express');
+var mysql = require('mysql');
+var util = require('util');
+
+var pool = mysql.createPool ({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    usser: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB_NAME,
+});
+
+pool.query = util.promisify(pool.query);
+
+module.export = pool;
